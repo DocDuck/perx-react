4e7f44bcdcc65a8f77406ccf4d63f3e7c4e9d6ed
Aleksandr Mitin <a.mitin@promo-bot.ru>
2020-09-16 18:38:55 +0500

    checkpoint

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,12 +4,13 @@ import './App.css';
 import User from '../user/User'
 import UserOrgs from '../userOrgs/UserOrgs.jsx'
 import UserRepos from '../userRepos/UserRepos.jsx'
+import UserInput from '../userInput/UserInput';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Perx is awesome!
+        <UserInput/>
         <div className="flex w100">
           <UserOrgs/>
           <UserRepos/>
--- src/components/userInput/UserInput.jsx
+++ src/components/userInput/UserInput.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { TextField, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
+const ValidationTextField = withStyles({
+    root: {
+      marginRight: '1rem',  
+      '& input:valid + fieldset': {
+        borderColor: 'green',
+        borderWidth: 2,
+      },
+      '& input:invalid + fieldset': {
+        borderColor: 'red',
+        borderWidth: 2,
+      },
+      '& input:valid:focus + fieldset': {
+        borderLeftWidth: 6,
+        padding: '4px !important', // override inline-style
+      },
+    },
+  })(TextField);
+
+const handleClick = () => {
+    console.log('click!')
+}  
 
 const UserInput = () => {
     return (
         <div className="flex">
-            <TextField
-                error
-                id="outlined-error-helper-text"
-                label="Error"
-                defaultValue="Hello World"
-                helperText="Incorrect entry."
+            <ValidationTextField
+                required
+                label="Enter github user"
+                defaultValue="DocDuck"
                 variant="outlined"
             />
-            <Button/>
+            <Button variant="contained" color="primary" onClick={() => handleClick()}>Find</Button>
         </div>
     )
 }
